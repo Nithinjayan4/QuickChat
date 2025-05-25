b7c20b308308e73ff414f3f7fc3bec9e8400159c
Nithin S J <nithinjayanjasla@gmail.com>
2025-05-25 16:11:48 +0530

    final commit vercel updates

--- server/server.js
+++ server/server.js
@@ -49,7 +49,14 @@ app.use("/api/messages", messageRouter);
 // connect to database
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log("Server is running on PORT:" + PORT);
-});
+if(process.env.NODE_ENV !== "production"){
+  const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log("Server is running on PORT:" + PORT
+));
+
+}
+
+// export server for vercel
+export default server
+
+
